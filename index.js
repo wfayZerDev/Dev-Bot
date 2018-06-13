a6647d934069285d6c760b4983e21b40a64a6b88
wfayZerDev <40219975+wfayZerDev@users.noreply.github.com>
2018-06-13 15:02:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client()
+var prefix = ("?"); 
 
-bot.on('message', function(message){
-    if (message.content === '..morgane'){
-        message.reply('j aime les ptn de vache comme momo')
+
+
+    bot.on('ready', () => {
+        bot.user.setPresence({ game: { name: '[?help] | Prefix', type: 0} });
+    
+    });
+
+
+bot.on('message', message => {
+    if(message.content ==="troll"){
+        message.reply("troll!!!");
+        console.log('troll troll');
     }
-})
-bot.on('message', function(message){
-    if (message.content === '..hugo'){
-        message.reply('j aime les ptn de chinois comme hugo')
+
+
+    if (message.content === prefix + "help"){
+        var help_embed = new Discord.RichEmbed()
+        .setColor('0x6F40A4')
+        .addField("💻Le site : http://forumtictacrp.mtxserv.fr/  💻")
+        .addField("🖥  Ip du serveur Tic-TacRp : 91.121.33.3 !  🖥")
+    message.channel.sendEmbed(help_embed)
+        console.log('help');
     }
-})
+
+});
+
+
+
 bot.on('message', function(message){
     if (message.content === '..tictac'){
         message.reply('🖥  Ip du serveur Tic-TacRp : 91.121.33.3 !  🖥  ')
     }
 })
 bot.on('message', function(message){
-    if (message.content === '..ip'){
+    if (message.content === '?ip'){
         message.reply('🖥  Ip du serveur Tic-TacRp : 91.121.33.3 !  🖥')
     }
 })
 bot.on('message', function(message){
-    if (message.content === '..site'){
+    if (message.content === '?site'){
         message.reply('💻  Le site est :http://forumtictacrp.mtxserv.fr/  💻')
     }
 })   
@@ -33,12 +52,6 @@ bot.on('message', function(message){
         ('*💻  Le site est : http://forumtictacrp.mtxserv.fr/  💻                                                                                🖥  Ip du serveur Tic-TacRp : 91.121.33.3 !  🖥*')
     }
 })   
-     
-bot.on('message', function(message){
-    if (message.content === '..marc'){
-        message.reply
-        ('Matheo Gerant Staff ! xD')
-    }
-}) 
+) 
 
 bot.login(process.env.TOKEN);
