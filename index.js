c8ddd33e7582a6a632cd623e23c699f28f27a2d8
wfayZerDev <40219975+wfayZerDev@users.noreply.github.com>
2018-07-15 18:11:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ bot.on('message', message => {
         .setColor("#15f153")
         .setTitle("|̲̅̅●̲̅̅|̲̅̅=̲̅̅|̲̅̅●̲̅̅| Score |̲̅̅●̲̅̅|̲̅̅=̲̅̅|̲̅̅●̲̅̅| ")
         .addField("France", "-> 2")
-        .addField("Croathie", "-> 1")
+        .addField("Croatie", "-> 1")
     message.channel.sendEmbed(help_embed)
         console.log('score');
     }
