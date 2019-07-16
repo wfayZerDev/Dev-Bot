e968e354b6c2fc317ee6bbe61658186e4d1eeccc
wfayZerDev <40219975+wfayZerDev@users.noreply.github.com>
2019-07-16 23:14:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -69,6 +69,8 @@ bot.on('message', message => {
         .addField("🔓Inscription🔓", "-> MP à @Lucius  ou @MrFriman  !")
         .addField("⌛️Pour les Absents⌛️ ", "-> Contacter a l'avance")
         .addField("🧾Scénario🧾", "-> Diamond limite")
+        .addField("🛠Config🛠", "-> 🎲Rôles : 12 minutes🎲")
+        .AddField("⚔️PvP⚔️", "-> 🔮15 minutes🔮")
     message.channel.sendEmbed(help_embed)
         console.log('info');
     }
